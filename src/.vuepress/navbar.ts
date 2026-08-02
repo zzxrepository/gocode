c056668aa6045a314e4695ff1e59be1174a2b808
mmzhang <zhangzhenxing@didiglobal.com>
2026-08-02 21:11:29 +0800

    gocode v1

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,12 +2,12 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   { text: "首页", icon: "house", link: "/" },
-  { text: "后端开发", icon: "server", link: "/backend/" },
-  { text: "算法与数据结构", icon: "diagram-project", link: "/algorithm/" },
   { text: "计算机基础", icon: "desktop", link: "/computer-fundamentals/" },
+  { text: "算法与数据结构", icon: "diagram-project", link: "/algorithm/" },
+  { text: "后端开发", icon: "server", link: "/backend/" },
+  { text: "前端开发", icon: "laptop-code", link: "/frontend/" },
   { text: "开发工具", icon: "screwdriver-wrench", link: "/tools/" },
   { text: "AI 应用开发", icon: "robot", link: "/ai-application-development/" },
-  { text: "前端开发", icon: "laptop-code", link: "/frontend/" },
   {
     text: "资源导航",
     icon: "compass",
